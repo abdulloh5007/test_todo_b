70c0dd7989fc13d6ca3acffaba6f09cafed3e318
Abdulloh Ergashev <122216752+abdulloh5007@users.noreply.github.com>
2023-12-27 17:21:42 +0500

    new

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const { v4: uuidv4 } = require('uuid');
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+    res.setHeader('Access-Control-Allow-Origin', 'https://my_todo_l1st.netlify.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
