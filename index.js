6b3b421ff6d5288b79913bbeab7ec7ebcf6c89e2
Abdulloh Ergashev <122216752+abdulloh5007@users.noreply.github.com>
2023-12-30 13:02:29 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://my-todo-l1st.netlify.app, https://main--my-todo-l1st.netlify.app/');
+    res.setHeader('Access-Control-Allow-Origin', 'https://my-todo-l1st.netlify.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
